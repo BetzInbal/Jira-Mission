48ad959aba2d83b9ff947c03184daf62df36a4e1
betz <betzinbal@gmail.com>
2024-09-01 09:46:02 +0300

    Add project files.

---
+++ Jira-Mission/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace Jira_Mission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            
+        }
+        public bool SearchingANumberWithinAnint(int[] bigNum, int num)
+        {
+
+            return bigNum.Contains(num) ? true : false;
+        }
+
+    }
+}
